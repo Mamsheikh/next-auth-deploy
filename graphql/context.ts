234977c40b2cfec5cc6443db135fa3d60750c5fe
Your Name <mamsheikh123@gmail.com>
2022-03-30 14:27:14 +0100

    📝

--- graphql/context.ts
+++ graphql/context.ts
@@ -2,14 +2,14 @@ import { NextApiResponse, NextApiRequest } from "next"
 import { PrismaClient } from "@prisma/client"
 import prisma from "../lib/prisma"
 import { getSession } from "next-auth/react"
-import { DefaultUser, User } from "next-auth"
+import { User } from "next-auth"
 
 export type Context = {
   prisma: PrismaClient
   req: NextApiRequest
   res: NextApiResponse
-  // user: DefaultUser | null
   email: string | null
+  // email: string;
   // user: User;
 }
 // const user = await prisma.user.findUnique({
@@ -26,14 +26,31 @@ export async function createContext({
   // const user = await prisma.user.findUnique({
   //   where: { email: session.user.email },
   // })
-  return {
-    req,
-    res,
-    prisma,
-    email: session?.user?.email,
+  const context: Context = {
+    ...req,
+    req: req,
+    res: res,
+    prisma: prisma,
+    email: null,
   }
+  if (session) {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: session?.user?.email,
+      },
+      rejectOnNotFound: true,
+    })
+    context.email = user.email
+  }
+
+  return context
 }
 
+// export type Context = {
+//   user: User;
+//   accessToken: string;
+//   prisma: PrismaClient;
+// };
 // export async function createContext({ req, res }): Promise<Context> {
 //   const { user, accessToken } = getSession(req, res);
 //   return {
